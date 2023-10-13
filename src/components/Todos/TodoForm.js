630c5440cbfaf707c45079014a963e2416e1cc31
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-10-13 13:31:22 +0300

    First commit

---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import styles from './TodoForm.module.css';
+console.log(styles)
+
+function TodoForm({ addTodo }) {
+    const [text, setText] = useState('');
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        addTodo(text);
+        setText('');
+    };
+
+    return (
+            <form className={styles.form} onSubmit={onSubmitHandler}>
+                <input
+                    className={styles.form__input}
+                    placeholder="Enter new todo"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                ></input>
+                <button className={styles.form__btn} type="submit">
+                    Submit
+                </button>
+            </form>
+    );
+}
+
+export default TodoForm;
