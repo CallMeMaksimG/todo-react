4674f16cacaaea97ceb06a14877910706a79bb1b
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-10-13 16:43:30 +0300

    Добавить отображение текста при пустом списке задач

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -2,13 +2,23 @@ import Todo from './Todo';
 import styles from './TodoList.module.css';
 
 function TodoList({ todos }) {
-    return (
-        <div className={styles.todoListContainer}>
+    if(!todos.length) {
+        return (
+            <div>
+                <h2>Todo list is empty</h2>
+            </div>
+        )
+    } else {
+        return (<div className={styles.todoListContainer}>
             {todos.map((todo, index) => (
                 <Todo key={index} todo={todo} />
             ))}
-        </div>
-    );
+        </div>)
+    }
+   
+    
+
+
 }
 
 export default TodoList;
